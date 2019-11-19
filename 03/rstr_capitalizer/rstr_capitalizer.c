d67e6fb5b759c3752a4132b36e749bb14fb1606e
swiqer <l.quarll@gmail.com>
2019-11-20 01:44:54 +0300

    20.11.19

--- 03/rstr_capitalizer/rstr_capitalizer.c
+++ 03/rstr_capitalizer/rstr_capitalizer.c
@@ -6,36 +6,47 @@ void    ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void    rstr_capitalizer(char *str)
+void rstr_capitalizer(char *str)
 {
     int i;
+    int len;
 
     i = 0;
     while(str[i] != '\0')
     {
-        while(str[i] &&(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)))
+        while(str[i] && (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)))
         {
             ft_putchar(str[i]);
             i++;
         }
-        if(str[i] >= 'a' && str[i] <= 'z')
-        {    
+        len = i;
+        while(str[len] && (str[len] != ' ' && (!(str[len] >= 9 && str[len] <= 13))))
+            len++;
+        len--;
+        while(str[i] && (str[i] != ' ' && (!(str[i] >= 9 && str[i] <= 13))) && i < len)
+        {
+            if(str[i] >= 'A' && str[i] <= 'Z')
+            {
+                ft_putchar(str[i] + 32);
+                i++;
+            }
+            else if(i < len)
+            {
+                ft_putchar(str[i]);
+                i++;
+            }
+        }
+        if(str[i] && (str[i] >= 'a' && str[i] <= 'z'))
+        {
             ft_putchar(str[i] - 32);
             i++;
         }
-        else if(str[i] >= 'A' && str[i] <= 'Z')
-        {    
+        else if(str[i])
+        {
             ft_putchar(str[i]);
             i++;
         }
-        while(str[i] && (str[i] != ' ' && (!(str[i] >= 9 && str[i] <= 13))))
-        {   
-            if(str[i] >= 'A' && str[i] <= 'Z')
-                ft_putchar(str[i] + 32);
-            else 
-                ft_putchar(str[i]);
-            i++;
-        }
+
     }
 }
 
@@ -47,9 +58,10 @@ int main(int ac, char **av)
         while(*av)
         {
             rstr_capitalizer(*av++);
-            write(1, "\n", 1);
+            write(1, "\n",1);
         }
     }
-    write(1, "\n", 1);
+    else 
+        write(1, "\n",1);
     return 0;
 }
